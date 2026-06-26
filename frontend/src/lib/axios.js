ef0da1f248b1c3fd6ecb585f8617172b170a9418
Zaid Attar <zaidattar2006@gmail.com>
2026-06-26 20:32:41 +0530

    final

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//const BASE_URL = import.meta.env.MODE == "development" ? "http://localhost:5090/api" : "/api"
 const api = axios.create({
-    baseURL : "http://localhost:5090/api"
+    baseURL : "/api",
 });
 
 export default api;
